a7f937ce78023e8ad54797a7c749f53ad5d4bf14
saurabhkale11 <101178695+saurabhkale11@users.noreply.github.com>
2025-01-02 13:34:12 +0530

    Add files via upload

---
+++ codecraft_4/script.js
@@ -0,0 +1,16 @@
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thank you, ${name}! Your message has been sent.`);
+    
+    e.target.reset();
+});
